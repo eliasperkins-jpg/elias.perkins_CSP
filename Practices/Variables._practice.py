8475c49d56db11f28591bbb658b85a3be42bfff1
eliasperkins-jpeg <elias.perkins@ucas-edu.net>
2025-09-02 13:46:37 -0600

    Work in Progress 1

--- Practices/Variables._practice.py
+++ Practices/Variables._practice.py
@@ -1,5 +1,43 @@
 # EP 6th Variables Practice\
 
-name = ("what is your name")
+name = input("what is your name\n")
 
-42 = ("write out 42")
+print("Hello", name)
+
+number1 = input("what is a number between 1 and 10\n")
+
+print("This is your" , number1 , "year as a vet")
+
+number2 = input("what is a number between 100 and 1000\n")
+
+print("you currently have" , number2 , "baby bunnies to feed")
+
+waffle = input("what was your breakfast today/n")
+
+print(waffle , s are awesome)
+
+input()
+
+print(yourfavoritecolor)
+
+input()
+
+print(whatistheschoolsname)
+
+input()
+
+print(whatistheyear)
+
+input()
+
+print(youreyecolor)
+
+input()
+
+print(whatisyourage)
+
+input()
+
+print(whatisyourfavoritesubjectinschool)
+
+input()
