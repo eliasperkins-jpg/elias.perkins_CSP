dcb812df6d8ab5a80de6a1461f1ab561e1bdfc44
eliasperkins-jpeg <elias.perkins@ucas-edu.net>
2025-09-04 13:44:22 -0600

    Notes Gallore

---
+++ Notes/expressions.py
@@ -0,0 +1,36 @@
+# EP 6th Expressions Notes\
+
+#What is an algorithm?
+    # Instructions that determines results for tasks
+
+# Sarah had 10 apples to give to six friends
+
+name = input("What is your name\n")
+print("Hello" , name)
+
+#algorithm for area
+length = 5 #variables
+width = 20
+area = length * width #process info
+print(length , "X" , width , "=" , area) #output
+
+age = 15
+age1 = 14
+age2 = 15
+age3 = 14
+total = age + age1 + age2 + age3
+average = (age + age1 + age2 + age3) / 4
+print("14 + 15 + 15 + 14 / 4 =")
+print("Average =" , average)
+
+#Math Equations
+num1 = 12
+num2 = 4
+num3 = 7
+print(num1 + num2) #Addition
+print(num1 - num2) #Subtraction
+print(num1 / num2) #Division
+print(num1 * num2) #Multiplication
+print(num1 // num2) #Exponents
+print(num1 ** num2) #Intiger Division
+print(num1 % num2) #Modulo (remainder)
