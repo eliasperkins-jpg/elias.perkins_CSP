d520edfd6cedf2809a20d9a15f6a109e1c7bdb90
Merp <elias.perkins@ucas-edu.net>
2025-09-07 11:59:54 -0600

    Update Expressions_practice2.py BEHOLD PERFECTION

--- Practices/Expressions_practice2.py
+++ Practices/Expressions_practice2.py
@@ -1,10 +1,50 @@
     #EP 6th Expressions Practice
 
-income = 4000
-groceries = 500
-rent = 1900
-savings = income * .1
+excome = 4000
+exoceries = 500
+exnt = 1900
+extertainment = 240
 
-rent + groceries = cost
+exvings = income * .1
+extertainment + groceries = cost
+exx = cost * .07
+exop_cost = tax + cost
+
+exmainder = income - (shop_cost + rent + savings)
+
+print(f"Your monthly usage is=" , remainder:.2)
+    #EP 6th Expressions Practice
+
+excome = 4000
+exoceries = 250
+exnt = 1900
+exilities = 580
+exs = 160
+
+exvings = excome * .1
+exst = exilities + exoceries + exs
+exop_cost = exs + exst
+
+exmainder = excome - (exop_cost + exnt + exvings)
+
+print(f"example you have {exmainder:.2f} left")
+
+print("Your monthly income is")
+income = float(input())
+print("Your monthly groceries cost is")
+groceries = float(input())
+print("Your rent is")
+rent = float(input())
+print("Your cost for utilities is")
+utilities = float(input())
+print("Gas cost is")
+gas = float(input())
+
+savings = income * .1
+cost = utilities + groceries + gas
 tax = cost * .07
-collective_cost = tax + cost
+shop_cost = tax + cost
+
+remainder = income - (shop_cost + rent + savings)
+
+print(f"You have {remainder:.2f} left")
