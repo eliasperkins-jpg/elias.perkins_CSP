5f3e55cb5653e0f3cb54e584d62f7bf456c6474a
eliasperkins-jpeg <elias.perkins@ucas-edu.net>
2025-09-08 13:47:11 -0600

    blep

---
+++ Notes/notes.strings.py
@@ -0,0 +1,13 @@
+    #EP 6th Strings Notes
+
+    #What does string men? its a collection of symbols held together by quotation marks.
+    #Where have we used strings? Whenever we use Print
+
+#examples
+first_name = input("What is your name/n").strip().title()#Strip includes, lower, upper, and title
+last_name = input("What is your name/n").strip().title()
+full_name = first_name + " " + last_name
+
+sentece = "The quick brown fox jumped over the lazy dog."
+
+print ("Welcome to my program" , full_name")
