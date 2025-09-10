abbf7b60b2edcf79bfb722246a4e99590c4de314
eliasperkins-jpeg <elias.perkins@ucas-edu.net>
2025-09-10 13:41:59 -0600

    DONZO

---
+++ Practices/Strings_Practice.py
@@ -0,0 +1,8 @@
+verb = input("Think of a random Verb")
+Adj = input("Think of a random Adjective")
+noun = input("Think of a random thing that moves")
+verbsequel = input("think of a random verb again")
+noun_sequel = input("Think of a random noun again")
+person = input("Think of a random Person")
+
+print("You were on a" , noun , "thinking of how you'd" , verb , person , "when you get to your stop." , "But before you get to your stop you see a" , noun_sequel , "." , "Being" , Adj , "you decide to" , verbsequel , "it")
