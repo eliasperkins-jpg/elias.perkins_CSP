d60b80e9116a9177052927b06bd6569f51942391
eliasperkins-jpeg <elias.perkins@ucas-edu.net>
2025-09-15 13:43:49 -0600

    One Second

---
+++ Notes/functions_practice.py
@@ -0,0 +1,49 @@
+# EP 6th Functions Notes
+
+#What a function is
+int = random(1)
+
+name =input("What is your name\n")
+print(f"Hello {name}!")
+      
+def welcome():
+    name = input("What is your name?\n")
+    print(f"Hello {name}!")
+
+print("The function is over")
+
+welcome()
+welcome()
+welcome()
+welcome()
+
+def add(number):
+    number = number + num_two
+    print(num_one)
+
+num_one = 71
+num_two = 10837349623083326376483764827952837498359
+add(num_one)
+
+
+def clean(info):
+    return info.strip().lower()
+
+name = input("what is your name?")
+quest = input ("what is your quest?")
+print("HELLO" , name , quest , "Person")
+
+def believe(sentence):
+    length = len(sentence)
+    spot_one = random(0, length -1)
+#Why we use functions
+
+#How to write a function in Python
+
+#What parameters and arguments are
+
+#How to use parameters and arguments in python
+
+#What return statements are
+
+#How to use return statements in a program
