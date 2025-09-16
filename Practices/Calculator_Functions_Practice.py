75de09d3ba5b1c7dc520035cc9a52be21280fae7
eliasperkins-jpeg <elias.perkins@ucas-edu.net>
2025-09-16 13:47:35 -0600

    LATER

--- Practices/Calculator_Functions_Practice.py
+++ Practices/Calculator_Functions_Practice.py
@@ -23,20 +23,25 @@ print(f"example you have {exmainder:.2f} left")
 
 def calculate(solution):
     print("Your monthly income is")
-    income = float(input())
-    print("Your monthly groceries cost is")
-    groceries = float(input())
-    print("Your rent is")
-    rent = float(input())
-    print("Your cost for utilities is")
-    utilities = float(input())
-    print("Gas cost is")
-    gas = float(input())
+    return float(input())
 
-    savings = income * .1
-    cost = utilities + groceries + gas
-    tax = cost * .07
-    shop_cost = tax + cost
+
+
+
+
+
+
+
+
+
+
+
+
+
+savings = income * .1
+cost = utilities + groceries + gas
+tax = cost * .07
+shop_cost = tax + cost
 
 remainder = income - (shop_cost + rent + savings)
 
