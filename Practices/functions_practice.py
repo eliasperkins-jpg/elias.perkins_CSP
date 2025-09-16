82bf7a9fc9c91c55290195404165374a2ba9aeb1
eliasperkins-jpeg <elias.perkins@ucas-edu.net>
2025-09-16 13:38:57 -0600

    Madame Antoinette

---
+++ Practices/functions_practice.py
@@ -0,0 +1,12 @@
+#EP 6th LaRose Hello World
+
+
+def hello(name):
+    name = input("What is your name?")
+    print("Hello" , name)
+   
+hello("Bill")
+hello("Eliza")
+hello("Maria")
+hello("Jorge")
+hello("Michaela")
