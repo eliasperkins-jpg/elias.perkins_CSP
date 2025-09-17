ae5cf819b2431fac358a903a03cc70d786f322da
eliasperkins-jpeg <elias.perkins@ucas-edu.net>
2025-09-17 13:44:09 -0600

    ME HAPPY HAPPY HAPPI

--- Notes/conditionals_notes.py
+++ Notes/conditionals_notes.py
@@ -1,6 +1,21 @@
 #EP 6th Conditionals Notes
 
-num = 77
+num = 44
 
-if num < 10: #Boolean Statement Condition
-    print(f"{num} is a single digit number")
+#if num < 10: #Boolean Statement Condition
+    #print(f"{num} is a single digit number")
+#else:
+    #print(f"{num} is not a single digit number")
+#if num < 100:
+    #print(f"{num} is a two or less digit number")
+
+    # == is equal, to < is less, > is more, <= less than or equal to, >= is more than or equal to, ! = NOT EQUAL
+
+name = input("Please tell us your name.")
+
+if name == "LaRose":
+    print("You are the teacher")
+elif name == "Tia":
+    print("you are the TA")
+else:
+    print(f"Hello {name}, you are a student!")
