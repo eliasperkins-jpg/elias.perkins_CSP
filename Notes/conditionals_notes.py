e41153824511d24ce6fea5ba4ea83d1e5dff51d2
eliasperkins-jpeg <elias.perkins@ucas-edu.net>
2025-09-18 13:24:05 -0600

    HELLO NEWBORK CITY

--- Notes/conditionals_notes.py
+++ Notes/conditionals_notes.py
@@ -1,6 +1,6 @@
 #EP 6th Conditionals Notes
 
-num = 44
+num = 24
 
 #if num < 10: #Boolean Statement Condition
     #print(f"{num} is a single digit number")
@@ -18,4 +18,16 @@ if name == "LaRose":
 elif name == "Tia":
     print("you are the TA")
 else:
+    if name == "Lucas":
+        print("You are in 7th period")
     print(f"Hello {name}, you are a student!")
+
+#AND OR & NOT
+if num > 0 and num < 10:
+    print(f"{num} is a single digit number")
+elif num < 25 or num == 50:
+    print(f"Wow!! {num} is a cool number bruh")
+elif not num < 100:
+    print(f" {num} is a large number")
+else:
+    print(f"You typed in {num}")
