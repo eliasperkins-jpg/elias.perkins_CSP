0899f4820ae41246b9981958ba3b794c319a37e6
eliasperkins-jpeg <elias.perkins@ucas-edu.net>
2025-09-22 13:05:23 -0600

    lips

--- Notes/for_loops.py
+++ Notes/for_loops.py
@@ -12,4 +12,14 @@ print(f"The hour is: {hour}")
 
 #lists
 
-siblings = ["Alex", "Katie", "Andrew", "Vienna", "Treyson", "Tia", "Treyson", "Xavier", "Jake" ["Joseph", "Noah", "Zee"], 8, 3.4158]
+siblings = ["Alex", "Katie", "Andrew", "Vienna", "Tia", "Treyson", "Xavier", "Jake"]
+
+print(siblings)
+#"Alex", "Katie", "Andrew", "Vienna", "Treyson", "Tia", "Treyson", "Xavier", "Jake"] WE DON'T WANT THIS
+
+print(siblings[3])
+
+print(siblings)
+siblings[4] = "Tia"
+siblings.remove("Vienna")
+print(siblings)
