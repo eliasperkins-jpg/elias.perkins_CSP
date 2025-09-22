c2ca07db4632707b7b201681f48a1dcfa84ccd00
eliasperkins-jpeg <elias.perkins@ucas-edu.net>
2025-09-22 13:41:05 -0600

    lemony water

--- Notes/for_loops.py
+++ Notes/for_loops.py
@@ -12,14 +12,56 @@ print(f"The hour is: {hour}")
 
 #lists
 
-siblings = ["Alex", "Katie", "Andrew", "Vienna", "Tia", "Treyson", "Xavier", "Jake"]
+siblings = ["dragofruity", "corny", "coconuffin", "banananananananana", "oran", "appeling", "grapebomb", "tangyrine", "lomony", "grapfrut"]
 
 print(siblings)
-#"Alex", "Katie", "Andrew", "Vienna", "Treyson", "Tia", "Treyson", "Xavier", "Jake"] WE DON'T WANT THIS
+#["dragofruity", "corny", "coconuffin", "banananananananana", "oran", "appeling", "grapebomb", "tangyrine", "lomony", "grapfrut"] WE DON'T WANT THIS
 
 print(siblings[3])
 
 print(siblings)
-siblings[4] = "Tia"
-siblings.remove("Vienna")
+siblings[4] = "oran"
+siblings.remove("banananananananana")
 print(siblings)
+
+#for loop
+for sibling in siblings:
+    #print(f"Hello {sibling}")
+    print("EAT")
+
+for x in range(20, -11, -1):
+    print(x)
+
+#while loops
+#infinite loops
+#while true:
+#   print("OH NO")  THIS WILL MELT HARD DRIVE
+
+#Good While Loop
+i = 1
+
+while i < 21:
+    if i % 2 == 0:
+        print(f"{i} is even")
+    else:
+        print(f"{i} is odd")
+    i += 1
+
+    import random
+
+    number = random.randint(1,20)
+    x = 1
+    """while x != number:
+        print("Ducky")
+        x += 1
+
+
+    print("Goose!")"""
+
+while True:
+    if number == x:
+        print("goose")
+        break
+    else:
+        print("DUCKY")
+        x +=1
