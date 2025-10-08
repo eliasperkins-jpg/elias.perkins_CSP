8f94ff9bb0dcbf79a84d9aa41d7dbfea129522a5
Merp <elias.perkins@ucas-edu.net>
2025-10-07 18:57:38 -0600

    Update variablesPractice.c YAYYY

---
+++ c__programming/PracticePRACTICE/variablesPractice.c
@@ -0,0 +1,22 @@
+// EP 6th Expressions Notes
+#include <stdio.h>
+#include <math.h>
+
+char name "Elias";
+char eyes "Hazel";
+char breakthoufast "None At All";
+char color "Somewhere between green and blue";
+char ucas "UCAS";
+char math "Math";
+
+float gpa "3.5";
+int seven "7";
+int year "2025";
+int age : "15"
+
+int main(){
+    printf("My name is %s \n", name);
+    printf("My name is %s \n", name);
+
+    return 0;
+}
