3b140fd4e01de528a7ee8b6b4d51b4bcebb9ec5e
Merp <elias.perkins@ucas-edu.net>
2025-10-07 19:12:47 -0600

    Update variablesPractice.c AGAIN

--- c__programming/PracticePRACTICE/variablesPractice.c
+++ c__programming/PracticePRACTICE/variablesPractice.c
@@ -16,7 +16,15 @@ int age : "15"
 
 int main(){
     printf("My name is %s \n", name);
-    printf("My name is %s \n", name);
-
+    printf("My age is %s \n", age);
+    printf("My eye color is is %s \n", eyes);
+    printf("My school is %s \n", ucas);
+    printf("My favorite subject is %s \n", math);
+    printf("My favorite color is %s \n", color);
+    printf("My breakfast today was %s \n", );
+    printf("My GPA could be %s \n", gpa);
+    printf("The nuber is %s \n", seven);
+    printf("The year is %s \n", year);
     return 0;
 }
+
