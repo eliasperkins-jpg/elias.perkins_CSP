93449be8ff7157e441eb68a17b1bb1a0ab557fab
Merp <elias.perkins@ucas-edu.net>
2025-10-08 22:32:36 -0600

    Update expressions.c

--- c__programming/PracticePRACTICE/expressions.c
+++ c__programming/PracticePRACTICE/expressions.c
@@ -2,13 +2,33 @@
 #include <stdio.h>
 #include <math.h>
 
-int tf = 24
+int tf = 24;
+int awful_pain = 22 / 2 - 2 * 5;
+int awful_pain_two = 4 - 6 / 6;
 
 int main(){ 
 
-    sol_uno = 7 - 24 / 8 * 4 + 6
+    int sol_uno = 7 - tf / 8 * 4 + 6;
+    printf("%d\n", sol_uno);
+    int sol_dos = 18 / 3 - 7 + 2 * 5;
+    printf("%d\n", sol_dos);
+    int sol_tres = 6 * 4 / 12 + 72 / 8 -9;
+    printf("%d\n", sol_tres);
+    int sol_quatro = (17 - 6 / 2) + 4 * 3;
+    printf("%d\n", sol_quatro);
+    int sol_cinco = -2 * (1 * 4 - 2 / 2) + (6 + 2 - 3);
+    printf("%d\n", sol_cinco);
+    int sol_seis = -1 * ((3 - 4 * 7) / 5) - 2 * 24 / 6; 
+    printf("%d\n", sol_seis);
+    int sol_siete = (3 * (5 * 5) / 15) - (5 - (2 * 2));
+    printf("%d\n", sol_siete);
+    int sol_ocho = ((1 * 1 * 1 * 1) * (2 * 2) + (3 * 3 * 3)) - (2 * 2 * 2 * 2 * 2) / 4;
+    printf("%d\n", sol_ocho);
+    int sol_nueve = (22 / 2 - 2 * 5) * awful_pain + (4 - 6 / 6) * awful_pain_two;
+    printf("%d\n", sol_nueve);
     
-
     
     return 0;
 }
+
+//printf("You should be saving about %f\n", savings);
