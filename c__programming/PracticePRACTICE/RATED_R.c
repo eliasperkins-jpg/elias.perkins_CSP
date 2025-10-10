b793f7bcead1895a39d697be1702adb4c9016ac8
Merp <elias.perkins@ucas-edu.net>
2025-10-10 16:25:05 -0600

    Create RATED_R.c

---
+++ c__programming/PracticePRACTICE/RATED_R.c
@@ -0,0 +1,19 @@
+
+int main(void){
+    int age;
+    printf("What is your age?");
+    scanf("%d", & age);
+
+    if (age>= 18){
+        printf("You can vote!\n")
+    }else if(age= 16){ 
+        printf("You can get a lisence!!\n");
+    }else if(grade= 15){ 
+        printf("You can get a permit!\n");
+    }else if (age= 6){
+        printf(" you can go to school! :(\n");
+    }else {
+        printf(" GET OFF OF THE COMPUTER BILLY! :(\n");
+    }
+
+    return 0;
