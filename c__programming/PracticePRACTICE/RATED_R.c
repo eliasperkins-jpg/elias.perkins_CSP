1f0a3c32a684490a0308f46be82a8ac3e0eaa6b5
eliasperkins-jpg <elias.perkins@ucas-edu.net>
2025-10-10 16:29:11 -0600

    RATEDRCOMINGTOTHEATRESNEARYOU

--- c__programming/PracticePRACTICE/RATED_R.c
+++ c__programming/PracticePRACTICE/RATED_R.c
@@ -1,19 +1,26 @@
+//EP 6th Old Enough Practice
+
+#include <stdio.h>
+#include <string.h>
 
 int main(void){
     int age;
     printf("What is your age?");
-    scanf("%d", & age);
+    scanf("%d", &age);
 
     if (age>= 18){
-        printf("You can vote!\n")
-    }else if(age= 16){ 
+        printf("You can vote!\n");
+    }else if (age == 16){ 
         printf("You can get a lisence!!\n");
-    }else if(grade= 15){ 
+    }else if (age == 15){ 
         printf("You can get a permit!\n");
-    }else if (age= 6){
+    }else if (age >= 6){
         printf(" you can go to school! :(\n");
     }else {
         printf(" GET OFF OF THE COMPUTER BILLY! :(\n");
     }
 
     return 0;
+
+
+}
