0c762567a609717d9620bc33d6c38d90f3c77aba
eliasperkins-jpg <elias.perkins@ucas-edu.net>
2025-10-13 13:44:53 -0600

    Wife'sMurderBallad

---
+++ c__programming/PracticePRACTICE/FIZZbuzz.c
@@ -0,0 +1,37 @@
+//EP 6th Loops and arrays notes
+
+#include <stdio.h>
+
+int main(void){
+
+    return 0;
+
+    int i = 1;
+
+    while(i < 51){
+        if i % 3 == 0 and i % 5 == 0++ 
+        printf("Duck\n");
+        count++;
+    }
+//
+
+
+
+
+/*
+
+i = 1
+
+while i < 51:
+    if i % 3 ==0 and i % 5 == 0:
+        print(f"{i} FIZZ BUZZZ")
+    elif i % 3 == 0:
+        print(f"{i} FIZZ")
+    elif 1 % 5 == 0:
+        print(f"{i} is BUZZZ")
+    else:
+        print(i)
+
+    i += 1
+
+*/
